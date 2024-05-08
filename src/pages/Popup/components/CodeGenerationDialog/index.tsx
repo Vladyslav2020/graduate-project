ff6280f9f9b087c23dd29dd0f916286a74d58a3c
VMoskalenko <vladyslav.moskalenko@microfocus.com>
2024-05-09 00:40:31 +0300

    Add auto scrolling when code is generating

--- src/pages/Popup/components/CodeGenerationDialog/index.tsx
+++ src/pages/Popup/components/CodeGenerationDialog/index.tsx
@@ -1,4 +1,4 @@
-import React, {useState} from 'react';
+import React, {useEffect, useRef, useState} from 'react';
 import ContentCopyRoundedIcon from '@mui/icons-material/ContentCopyRounded';
 import {Box, Button, Dialog, DialogActions, DialogContent, DialogTitle, IconButton, TextField} from '@mui/material';
 import {CopyToClipboard} from 'react-copy-to-clipboard';
@@ -15,6 +15,7 @@ export const CodeGenerationDialog = ({testCase, open, setOpen}: CodeGenerationDi
     const [generatedCode, setGeneratedCode] = useState('');
     const [codeGenerating, setCodeGenerating] = useState(false);
     const [language, setLanguage] = useState('JavaScript');
+    const textFieldRef = useRef<HTMLDivElement>(null);
 
     const handleClose = () => {
         setOpen(false);
@@ -25,6 +26,13 @@ export const CodeGenerationDialog = ({testCase, open, setOpen}: CodeGenerationDi
         await aiGenerationService.getCodeForTestCase(testCase, setGeneratedCode);
         setCodeGenerating(false);
     };
+
+    useEffect(() => {
+        if (textFieldRef.current) {
+            const textarea = textFieldRef.current.querySelector('textarea') as HTMLTextAreaElement;
+            textarea.scrollTop = textarea.scrollHeight;
+        }
+    }, [generatedCode]);
 
     return (
         <Dialog open={open} onClose={handleClose} maxWidth="md" fullWidth>
@@ -41,6 +49,7 @@ export const CodeGenerationDialog = ({testCase, open, setOpen}: CodeGenerationDi
                 </Box>
                 <Box mt={2} position="relative">
                     <TextField
+                        ref={textFieldRef}
                         label="Generated Code"
                         multiline
                         rows={10}
